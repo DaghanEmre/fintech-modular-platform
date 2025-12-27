c05bc4301062c19ead5d2462af228fb4c8f6e93f
Daghan Emre Aytac <daghanemreaytac@hotmail.com>
2025-12-27 15:37:23 +0300

    Add customer-service skeleton with clean architecture structure

---
+++ services/customer-service/src/main/java/com/daghanemre/fintech/customer/CustomerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.daghanemre.fintech.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+}
